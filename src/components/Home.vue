433f9ea063d790ae84d70317a50478be2cb87a9e
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2020-10-31 11:59:44 +0530

    add bootstrap and create login page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,32 @@
 <template lang="pug">
-  div homepage, coming soon !!!! this page will have the login form
+  div
+    b-container(fluid='sm')
+      b-col(align-self='center' cols="5")
+        b-form(@submit='onSubmit')
+          b-form-group(label='Login ID:' label-for='login')
+            b-form-input#login(v-model='form.id' type='text' required=true placeholder='Login')
+          b-form-group#input-group-2(label='Password:' label-for='password')
+            b-form-input#password(v-model='form.password' required=true placeholder='Password')
+          b-button(type='submit' variant='primary') Submit
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      form: {
+        id: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit: function (event) {
+      event.preventDefault()
+      // do some validation and go to next page
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home'
+import IdCard from '../components/IdCard'
+import InformationForm from '../components/InformationForm'
 
 Vue.use(Router)
 
@@ -12,9 +14,14 @@ export default new Router({
       component: Home
     },
     {
-      path: '/',
-      name: 'Home',
-      component: Home
+      path: '/information-form',
+      name: 'InformationForm',
+      component: InformationForm
+    },
+    {
+      path: '/id-card',
+      name: 'IdCard',
+      component: IdCard
     }
   ]
 })
