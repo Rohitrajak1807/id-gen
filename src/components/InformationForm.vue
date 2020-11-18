d447cf12d4059ab8c8a7ff34ec9f648899828fe8
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2020-11-19 01:03:13 +0530

    create student now working

--- src/components/InformationForm.vue
+++ src/components/InformationForm.vue
@@ -6,32 +6,32 @@
         b-row
           b-col
             b-form-group#form_group_full_name(label='Full Name' label-for='full_name')
-              b-form-input#full_name(v-model='fullName' placeholder='Enter your Full Name' trim='')
+              b-form-input#full_name(v-model='formData.fullName' placeholder='Enter your Full Name' trim=true)
         b-row
           b-col
             b-form-group#form_group_roll_number(label='Roll Number' label-for='roll_number')
-              b-form-input#roll_number(v-model='rollNumber' placeholder='Enter your Roll Number' trim='')
+              b-form-input#roll_number(v-model='formData.rollNumber' placeholder='Enter your Roll Number' trim=true)
           b-col
             b-form-group#form_group_course(description='Choose your course' label='Course' label-for='course')
-              b-form-select#course(v-model='course' :options='courseOptions')
+              b-form-select#course(v-model='formData.course' :options='courseOptions')
           b-col
             b-form-group#form_group_department(description='Choose your Department' label='Department' label-for='department')
-              b-form-select#department(v-model='department' :options='departmentOptions')
+              b-form-select#department(v-model='formData.department' :options='departmentOptions')
         b-row
           b-col
             b-form-group#form_group_date_of_birth(label='Date of Birth' label-for='date_of_birth')
-              b-form-datepicker#date_of_birth(v-model='dateOfBirth' placeholder='Enter your Date of Birth')
+              b-form-datepicker#date_of_birth(v-model='formData.dateOfBirth' placeholder='Enter your Date of Birth')
           b-col
             b-form-group#form_group_phone_number(label='Phone Number' label-for='phone_number')
-              b-form-input#phone_number(v-model='phone' placeholder='Enter your Phone Number' type='number' trim='')
+              b-form-input#phone_number(v-model='formData.phone' placeholder='Enter your Phone Number' type='number' trim=true)
         b-row
           b-col
             b-form-group#form_group_institutedateOfBirthSettings_e_mail(label='Institute E-mail' label-for='institute_e_mail')
-              b-form-input#institute_e_mail(v-model='instituteEMail' placeholder='Enter your Institutional E-mail ID' type='email' trim='')
+              b-form-input#institute_e_mail(v-model='formData.instituteEMail' placeholder='Enter your Institutional E-mail ID' type='email' trim=true)
         b-row
           b-col
             b-form-group#form_group_permanent_address(label='Permanent Address' label-for='permanent_address')
-              b-form-textarea#permanent_address(v-model='permanentAddress' placeholder='Enter your Permanent Address' trim='')
+              b-form-textarea#permanent_address(v-model='formData.permanentAddress' placeholder='Enter your Permanent Address' trim=true)
         b-row.ml-1
           b-button(type='submit' variant='primary' v-on:click='onSubmit') Submit
 </template>
@@ -46,31 +46,34 @@ export default {
   },
   data () {
     return {
-      fullName: '',
-      rollNumber: '',
-      phone: '',
-      instituteEMail: '',
-      permanentAddress: '',
-      course: null,
+      formData: {
+        fullName: '',
+        rollNumber: '',
+        phone: '',
+        instituteEmail: '',
+        permanentAddress: '',
+        course: null,
+        department: null,
+        dateOfBirth: ''
+      },
       courseOptions: [
         {value: 'phd', text: 'PhD'},
         {value: 'mtech', text: 'M.Tech'},
         {value: 'btech', text: 'B.Tech'}
       ],
-      department: null,
       departmentOptions: [
         {value: 'cse', text: 'Computer Science and Engineering'},
         {value: 'ece', text: 'Electronics and Communication Engineering'},
         {value: 'eee', text: 'Electric and Electronics Engineering'},
         {value: 'me', text: 'Mechanical Engineering'},
         {value: 'ce', text: 'Civil Engineering'}
-      ],
-      dateOfBirth: ''
+      ]
     }
   },
   methods: {
-    onSubmit () {
-      console.log(this.$data)
+    onSubmit: async function () {
+      const response = await this.axios.post('http://localhost:3000/students', this.formData)
+      console.log(response)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.use(BootstrapVue)
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
